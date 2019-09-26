af8f332fdec105001f5ae44e5b01460b4bf1c7d1
Max Zakzook <mzakzook@me.com>
2019-09-26 11:45:59 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,93 @@
 require_relative "../config/environment.rb"
 
 class Student
-
+  attr_accessor :id, :name, :grade
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+  def self.create_table
+    sql =  <<-SQL 
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      grade TEXT
+      )
+      SQL
+  DB[:conn].execute(sql) 
+  end
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
+      SQL
+      DB[:conn].execute(sql)
+  end
+  def save
+    if !@id
+      sql = <<-SQL
+        INSERT INTO students (name, grade) 
+        VALUES (?, ?)
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      self.assign_id
+    else
+      sql = <<-SQL
+        UPDATE students SET name = ?, grade = ?
+        WHERE id = ?
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
+    end
+    
+  end
+
+  def assign_id 
+    if @id == nil
+      sql = <<-SQL
+        SELECT id FROM students
+        ORDER BY id DESC
+        LIMIT 1
+      SQL
+      @id = (DB[:conn].execute(sql))[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    new_stu = Student.new(name, grade)
+    new_stu.save
+    new_stu
+  end
+
+  def self.new_from_db(row)
+    new_student = Student.new(row[0], row[1], row[2])
+    new_student  
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE name = ?
+        LIMIT 1
+      SQL
+   
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
+
+  end
+
+  def update
+    sql = <<-SQL
+        UPDATE students SET name = ?, grade = ?
+        WHERE id = ?
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
 
 
+  end
 end
